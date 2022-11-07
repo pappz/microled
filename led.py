Fix fade calculation

led.py
@@ -9,7 +9,6 @@ except:
 
 
 def hsl_to_rgb(i_h, i_s, i_l):
-
     h = (i_h % 360) / 360.0
     s = i_s / 100.0
     l = i_l / 100.0
@@ -18,7 +17,6 @@ def hsl_to_rgb(i_h, i_s, i_l):
         r = g = b = 255 * l
         return int(r), int(g), int(b)
 
-    t1 = 0
     if l < 0.5:
         t1 = l * (1.0 + s)
     else:
@@ -62,17 +60,19 @@ class Led:
         self.demo()
 
     def fade(self, r, g, b, max_steps=50, sleep_time=None):
+
         diff = [0] * 3
         diff[0] = self.color[0] - r
         diff[1] = self.color[1] - g
         diff[2] = self.color[2] - b
         n_steps = self.__calc_steps(max_steps, diff)
 
-        for i in range(0, 3):
-            diff[i] = int(diff[i] / n_steps)
-
-        for i in range(1, n_steps+1):
-            self.color = (self.color[0] - diff[0], self.color[1] - diff[1], self.color[2] - diff[2])
+        new_color = [0] * 3
+        for i in range(1, n_steps):
+            new_color[0] = self.color[0] - int(i * diff[0] / n_steps)
+            new_color[1] = self.color[1] - int(i * diff[1] / n_steps)
+            new_color[2] = self.color[2] - int(i * diff[2] / n_steps)
+            self.color = tuple(new_color)
             self.np.fill(self.color)
             self.np.write()
             if sleep_time is not None:
@@ -86,16 +86,18 @@ class Led:
         l = 30
         while True:
             h = self.__random_range(0, 360)
-            s = self.__random_range(0, 100)
+            s = self.__random_range(70, 80)
             color = hsl_to_rgb(h, s, l)
             self.fade(color[0], color[1], color[2], max_steps=255, sleep_time=self.__random_range(0.3, 0.5))
-            time.sleep(self.__random_range(15, 30))
+            time.sleep(self.__random_range(7, 13))
 
-    def __calc_steps(self, max_steps, diff):
+    @staticmethod
+    def __calc_steps(max_steps, diff):
         highest_diff = max(abs(i) for i in diff)
         return min([highest_diff, max_steps])
 
-    def __random_range(self, i, j):
+    @staticmethod
+    def __random_range(i, j):
         b = getrandbits(16)
         diff = abs(j - i)
         ration = b / 65535