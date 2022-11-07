48cd2fa958ecef31c8d32d44afe1432e9f16c301
pzoli <zoltan.pmail@gmail.com>
2022-11-07 13:18:17 +0100

    New approach

--- m.py
+++ m.py
@@ -1,31 +1,94 @@
+import asyncio
+import time
+from threading import Thread
+from lib.queue import Queue
 
-color = [1,2,3]
+task_list = []
+
+async def async_func():
+    print('1. Velotio ...')
+    await asyncio.sleep(2)
+    print('1. ... Blog!')
 
 
-def __calc_steps(max_steps, diff):
-    highest_diff = max(abs(i) for i in diff)
-    return min([highest_diff, max_steps])
+async def async_func2():
+    print('2. Velotio ...')
+    await asyncio.sleep(2)
+    print('2. ... Blog!')
 
 
-def fade(r, g, b, max_steps=50, sleep_time=None):
-    global color
-    diff = [0] * 3
+async def start(tasks):
+    while True:
+        for t in tasks:
+            print("new task")
+            await t
+        time.sleep(0.1)
 
-    diff[0] = color[0] - r
-    diff[1] = color[1] - g
-    diff[2] = color[2] - b
 
-    n_steps = __calc_steps(max_steps, diff)
+def looper(tasks):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(start(tasks))
+    # asyncio.run(start(tasks))
 
-    for i in range(0, 3):
-        diff[i] = int(diff[i] / n_steps)
 
-    for i in range(1, n_steps+1):
-        color = (color[0] - diff[0], color[1] - diff[1], color[2] - diff[2])
-        '''
-        if sleep_time is not None:
-            time.sleep(sleep_time)
-        '''
-    color = (r, g, b)
+def main():
+    print("starting thread")
+    t = Thread(target=looper, args=(task_list,), daemon=True)
+    t.start()
+    time.sleep(1)
 
-fade(255, 255, 255, 10)
+
+class LedService:
+    active_task = None
+    loop = asyncio.get_event_loop()
+
+    queue = Queue(1)
+
+    def __init__(self):
+        t = Thread(target=self.__looper, daemon=True)
+        t.start()
+
+    def __looper(self):
+        # self.loop = asyncio.new_event_loop()
+        # asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self.__handler())
+
+    async def __handler(self):
+        while True:
+            task = self.queue.get()
+            await task
+
+    def demo(self):
+        task = self.loop.create_task(self.__start_demo())
+        print("write to queue")
+        self.active_task = task
+        self.queue.put(task)
+
+    def off(self):
+        self.stop()
+        task = self.loop.create_task(self.__off())
+        self.queue.put(task)
+
+    async def __start_demo(self):
+        while True:
+            for i in range(1, 5):
+                print(i)
+                time.sleep(1)
+            print("this is demo")
+            await asyncio.sleep(1)
+
+    def stop(self):
+        self.active_task.cancel()
+
+    async def __off(self):
+        print("off")
+
+
+s = LedService()
+s.demo()
+time.sleep(2)
+print("try to stop")
+s.off()
+print("sleep 10")
+time.sleep(10)
